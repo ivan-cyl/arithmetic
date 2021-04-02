6e56995ab72563d546a625032d7c95df8dc3db7a
ivan-cyl <78088602+ivan-cyl@users.noreply.github.com>
2021-04-02 11:04:22 +0800

    Add files via upload

--- algorithm-java/Sorts/src/KthSmallest.java
+++ algorithm-java/Sorts/src/KthSmallest.java
@@ -1,2 +1,43 @@
 public class KthSmallest {
+    private static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private static int partition(int[] arr, int p, int r) {
+        int pivot = arr[r];
+        int i = p;
+        for (int j = p; j < r; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, r);
+        return i;
+    }
+
+    public static int KthSmallest(int[] arr, int k) {
+        if (arr == null || arr.length < k) {
+            return -1;
+        }
+        int partition = partition(arr, 0, arr.length - 1);
+        while (partition + 1 != k) {
+            if (partition + 1 < k) {
+                partition = partition(arr, partition + 1, arr.length);
+            } else {
+                partition = partition(arr, 0, partition - 1);
+            }
+        }
+        return arr[partition];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 7, 6, 1, 3};
+        System.out.println(KthSmallest(arr, 3));
+    }
 }
