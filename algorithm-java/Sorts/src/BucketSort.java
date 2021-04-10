b276cfab1d7e7a285dfbd4990b3b1810b142bdb6
ivan-cyl <78088602+ivan-cyl@users.noreply.github.com>
2021-04-10 15:02:28 +0800

    Add files via upload

--- algorithm-java/Sorts/src/BucketSort.java
+++ algorithm-java/Sorts/src/BucketSort.java
@@ -1,3 +1,78 @@
 public class BucketSort {
+    private static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private static void ensureCapacity(int[][] buckets, int bucketIndex) {
+        int[] tempArr = buckets[bucketIndex];
+        int[] newArr = new int[tempArr.length * 2];
+        for (int i = 0; i < tempArr.length; i++) {
+            newArr[i] = tempArr[i];
+        }
+        buckets[bucketIndex] = newArr;
+    }
+
+    private static int partition(int[] arr, int p, int r) {
+        int pivot = arr[r];
+        int i = p;
+        for (int j = p; j < r; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+        swap(arr, i, r);
+        return i;
+    }
+
+    private static void quickSort(int[] arr, int p, int r) {
+        if (p >= r) {
+            return;
+        }
+        int q = partition(arr, p, r);
+        quickSort(arr, p, q - 1);
+        quickSort(arr, q + 1, r);
+    }
+
+    public static void bucketSort(int[] arr, int bucketSize) {
+        if (arr.length < 2) {
+            return;
+        }
+        int minValue = arr[0];
+        int maxValue = arr[1];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minValue) {
+                minValue = arr[i];
+            } else if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+
+        int bucketCount = (maxValue - minValue) / bucketSize + 1;
+        int[][] buckets = new int[bucketCount][bucketSize];
+        int[] indexArr = new int[bucketCount];
+        for (int i = 0; i < arr.length; i++) {
+            int bucketIndex = (arr[i] - minValue) / bucketSize;
+            if (indexArr[bucketIndex] == buckets[bucketIndex].length) {
+                ensureCapacity(buckets, bucketIndex);
+            }
+            buckets[bucketIndex][indexArr[bucketIndex]++] = arr[i];
+        }
+
+        int k = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            if (indexArr[i] == 0) {
+                continue;
+            }
+            quickSort(buckets[i], 0, indexArr[i]);
+            for (int j = 0; j < indexArr[i]; j++) {
+                arr[k++] = buckets[i][j];
+            }
+        }
+    }
 }
- 
