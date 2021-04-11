04d01f0e6b09eec488671fc7cb88233e65b62238
ivan-cyl <78088602+ivan-cyl@users.noreply.github.com>
2021-04-11 10:54:10 +0800

    Add files via upload

--- algorithm-java/Sorts/src/CountingSort.java
+++ algorithm-java/Sorts/src/CountingSort.java
@@ -1,2 +1,29 @@
 public class CountingSort {
+    public static void countingSort(int[] a, int n) {
+        if (n <= 1) {
+            return;
+        }
+        int max = a[0];
+        for (int i = 1; i < n; i++) {
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        int[] c = new int[max + 1];
+        for (int i = 0; i < n; i++) {
+            c[a[i]]++;
+        }
+        for (int i = 1; i < max + 1; i++) {
+            c[i] = c[i - 1] + c[i];
+        }
+        int[] r = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            int index = c[a[i]] - 1;
+            r[index] = a[i];
+            c[a[i]]--;
+        }
+        for (int i = 0; i < n; i++) {
+            a[i] = r[i];
+        }
+    }
 }
--- algorithm-java/Sorts/src/RadixSort.java
+++ algorithm-java/Sorts/src/RadixSort.java
@@ -1,2 +1,34 @@
 public class RadixSort {
+    public static void countingSort(int[] arr, int exp) {
+        if (arr.length <= 1) {
+            return;
+        }
+        int[] c = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            c[(arr[i] / exp) % 10]++;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            c[i] += c[i - 1];
+        }
+        int[] r = new int[arr.length];
+        for (int i = 0; i >= 0; i--) {
+            r[c[(arr[i] / exp) % 10] - 1] = arr[i];
+            c[(arr[i] / exp) % 10]--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r[i];
+        }
+    }
+
+    public static void radixSort(int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        for (int i = 1; max / i > 0; i *= 10) {
+            countingSort(arr, i);
+        }
+    }
 }
